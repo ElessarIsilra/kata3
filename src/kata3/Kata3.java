3d31b3867c86694f743e4374b7ddf0414e74c891
Elessar2 <Elessar2@Elessar2-PC>
2016-01-07 18:34:43 +0000

    Kata 3 v Final

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,30 @@
+package kata3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Kata3 {
+
+    public static void main(String[] args) {
+        Histogram<String> histogram = new Histogram <>();
+        String name = "C:\\Users\\Elessar2\\Desktop\\emailsfilev1.txt";
+        try{
+            BufferedReader fileIn = new BufferedReader(new FileReader(name));
+            String mail;
+            
+            while((mail = fileIn.readLine())!= null){
+                if(mail.contains("@")){
+                    histogram.increment(mail.split("@")[1]);
+                }
+            }
+            
+            
+            
+        }catch(Exception e){
+            
+        }
+        HistogramDisplay histograma = new HistogramDisplay(histogram);
+        histograma.execute();
+    }
+    
+}
